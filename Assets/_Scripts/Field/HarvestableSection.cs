88e2d44aef6580823d0102a2bba2ad0b4707ecc9
lentarat <lntr.exe@gmail.com>
2026-07-02 22:59:13 +0300

    Crops textures correct PPU, harvestable area with fences.

---
+++ Assets/_Scripts/Field/HarvestableSection.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HarvestableSection : MonoBehaviour
+{
+    [SerializeField] private float _edgeCount;
+    [SerializeField] private HarvestableTile _cropTile;
+}
